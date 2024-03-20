dae71a8a9f8469639b8487a996b05cded8cf5c2a
NA-Siddiky <siddiky.academic@gmail.com>
2024-03-21 01:49:15 +0600

    adding initial corepack config using yarn for monorepo

---
+++ apps/android/unzip_blog_app/textComponent.tsx
@@ -0,0 +1,39 @@
+import {Content} from '@soppiya/elemix';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import {Text} from 'react-native';
+
+// Define your Client Component
+const FetchPosts = () => {
+  // Initialize state to store the posts
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Function to fetch posts from the API
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts',
+        );
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Failed to fetch posts:', error);
+      }
+    };
+
+    // Call the fetchPosts function
+    fetchPosts();
+  }, []); // Empty dependency array means this effect runs once on component mount
+
+  return (
+    <>
+      <Text>Posts</Text>
+      <Text>{Content.merge(['c', 'r', 't'], '0')}</Text>
+      {posts.map((post: any) => (
+        <Text>{post.title}</Text>
+      ))}
+    </>
+  );
+};
+
+export default FetchPosts;
